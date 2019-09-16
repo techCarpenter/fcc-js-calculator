8b0aa57db71659e11237ef9c1a834633d6d0558d
techCarpenter <bdevries95@gmail.com>
2019-09-15 22:34:02 -0400

    Add onClick functions for various button types

--- src/index.js
+++ src/index.js
@@ -1,104 +1,76 @@
+//@ts-check
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import * as math from "mathjs";
+
+/* VARS */
+const lastPressedEnum = {
+    OPS: 1,
+    NUM: 2,
+    EQUAL: 3,
+    DEC: 4,
+    CLEAR: 5
+  },
+  endsWithOperator = /\+|\/|\*|-$/;
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      arg1: "",
-      arg2: "",
-      result: "",
-      input: "0",
-      output: "",
-      operator: { display: "", type: "none" }
-    };
-    this.handleClick = this.handleClick.bind(this);
+    this.state = { expr: "", currentArg: "0", result: "", output: "", lastPressed: lastPressedEnum.CLEAR };
+    this.handleClear = this.handleClear.bind(this);
+    this.handleDigit = this.handleDigit.bind(this);
+    this.handleOperator = this.handleOperator.bind(this);
+    this.handleDecimal = this.handleDecimal.bind(this);
+    this.handleEvaluate = this.handleEvaluate.bind(this);
   }
-  handleClick(val) {
-    let localState = this.state;
-
-    val = val.toLowerCase();
-    switch (val) {
-      case "ac":
-        localState.input = "0";
-        localState.output = "0";
-        localState.operator = { display: "", type: "none" };
-        break;
-      case "0":
-      case "1":
-      case "2":
-      case "3":
-      case "4":
-      case "5":
-      case "6":
-      case "7":
-      case "8":
-      case "9":
-        if (localState.input === "0") {
-          localState.input = "";
-        }
-        if (localState.input.length < 15) {
-          localState.input = localState.input + val;
-        }
-        break;
-      case ".":
-        if (!/\./g.test(localState.input)) {
-          localState.input = localState.input + val;
-        }
-        break;
-      case "+":
-      case "-":
-      case "*":
-      case "/":
-        localState.operator =
-          val === "+"
-            ? { display: "+", type: "add" }
-            : val === "-"
-            ? { display: "-", type: "sub" }
-            : val === "*"
-            ? { display: "*", type: "mul" }
-            : { display: "/", type: "div" };
-        localState.arg1 = localState.input;
-        localState.input = "0";
-        break;
-      case "=": //Calculate!
-        localState.arg2 = localState.input;
-        localState.input = "0";
-        let result = 0;
-        switch (localState.operator.type) {
-          case "add":
-            result = parseInt(localState.arg1) + parseInt(localState.arg2);
-            break;
-          case "sub":
-            result = parseInt(localState.arg1) - parseInt(localState.arg2);
-            break;
-          case "mul":
-            result = parseInt(localState.arg1) * parseInt(localState.arg2);
-            break;
-          case "div":
-            result = parseInt(localState.arg1) / parseInt(localState.arg2);
-            break;
-          default:
-            break;
-        }
-
-        console.log("result = " + result);
-        if (localState.arg1 > 0 && localState.arg2 > 0) {
-          localState.output =
-            localState.arg1 +
-            localState.operator.display +
-            localState.arg2 +
-            "=" +
-            result;
-        }
-        break;
-      default:
-        console.log(val);
-        break;
+  handleClear() {
+    this.setState({ expr: "", currentArg: "0", result: "", output: "0", lastPressed: lastPressedEnum.CLEAR });
+  }
+  handleEvaluate(event) {
+    let stateObj = {};
+    if (this.state.lastPressed != lastPressedEnum.EQUAL) {
+      stateObj.expr = this.state.expr + this.state.currentArg;
+      stateObj.currentArg = "0";
+      stateObj.result = math.evaluate(stateObj.expr);
+      stateObj.output = stateObj.expr + "=" + stateObj.result;
     }
-    console.log(localState);
-    this.setState(localState);
+
+    stateObj.lastPressed = lastPressedEnum.EQUAL;
+    this.setState(stateObj);
+  }
+  handleDecimal(event) {
+    let stateObj = {};
+    if (!this.state.currentArg.includes(".")) {
+      stateObj.currentArg = this.state.currentArg + ".";
+    }
+    stateObj.lastPressed = lastPressedEnum.DEC;
+    this.setState(stateObj);
+  }
+  handleOperator(event) {
+    let stateObj = {};
+    if (this.state.lastPressed === lastPressedEnum.EQUAL) {
+      stateObj.expr = this.state.result + event.target.value;
+      stateObj.result = "";
+    } else if (endsWithOperator.test(this.state.expr)) {
+      stateObj.expr = this.state.expr.substring(0, this.state.expr.length - 1) + event.target.value;
+    } else {
+      stateObj.expr = this.state.expr + this.state.currentArg + event.target.value;
+    }
+    stateObj.lastPressed = lastPressedEnum.OPS;
+    stateObj.currentArg = "0";
+    stateObj.output = stateObj.expr;
+    this.setState(stateObj);
+  }
+  handleDigit(event) {
+    let stateObj = { currentArg: this.state.currentArg };
+    if (this.state.currentArg === "0") {
+      stateObj.currentArg = "";
+    }
+    stateObj.currentArg += event.target.value;
+    stateObj.lastPressed = lastPressedEnum.NUM;
+    stateObj.output = this.state.expr + stateObj.currentArg;
+    this.setState(stateObj);
   }
   render() {
     return (
@@ -109,25 +81,25 @@ class App extends React.Component {
         <div className="container">
           <div id="total-display">
             <div id="display">{this.state.output}</div>
-            <div id="lower-display">{this.state.input}</div>
+            <div id="lower-display">{this.state.currentArg}</div>
           </div>
-          <Button id="clear" value="AC" onClick={this.handleClick} />
-          <Button id="divide" value="/" onClick={this.handleClick} />
-          <Button id="multiply" value="*" onClick={this.handleClick} />
-          <Button id="subtract" value="-" onClick={this.handleClick} />
-          <Button id="add" value="+" onClick={this.handleClick} />
-          <Button id="nine" value="9" onClick={this.handleClick} />
-          <Button id="eight" value="8" onClick={this.handleClick} />
-          <Button id="seven" value="7" onClick={this.handleClick} />
-          <Button id="six" value="6" onClick={this.handleClick} />
-          <Button id="five" value="5" onClick={this.handleClick} />
-          <Button id="four" value="4" onClick={this.handleClick} />
-          <Button id="three" value="3" onClick={this.handleClick} />
-          <Button id="two" value="2" onClick={this.handleClick} />
-          <Button id="one" value="1" onClick={this.handleClick} />
-          <Button id="equals" value="=" onClick={this.handleClick} />
-          <Button id="decimal" value="." onClick={this.handleClick} />
-          <Button id="zero" value="0" onClick={this.handleClick} />
+          <Button id="clear" value="AC" onClick={this.handleClear} />
+          <Button id="divide" value="/" onClick={this.handleOperator} />
+          <Button id="multiply" value="*" onClick={this.handleOperator} />
+          <Button id="subtract" value="-" onClick={this.handleOperator} />
+          <Button id="add" value="+" onClick={this.handleOperator} />
+          <Button id="nine" value="9" onClick={this.handleDigit} />
+          <Button id="eight" value="8" onClick={this.handleDigit} />
+          <Button id="seven" value="7" onClick={this.handleDigit} />
+          <Button id="six" value="6" onClick={this.handleDigit} />
+          <Button id="five" value="5" onClick={this.handleDigit} />
+          <Button id="four" value="4" onClick={this.handleDigit} />
+          <Button id="three" value="3" onClick={this.handleDigit} />
+          <Button id="two" value="2" onClick={this.handleDigit} />
+          <Button id="one" value="1" onClick={this.handleDigit} />
+          <Button id="zero" value="0" onClick={this.handleDigit} />
+          <Button id="equals" value="=" onClick={this.handleEvaluate} />
+          <Button id="decimal" value="." onClick={this.handleDecimal} />
         </div>
         <div style={{ textAlign: "center" }}>
           <p>
@@ -136,8 +108,7 @@ class App extends React.Component {
               id="portfolio-link"
               href="https://techcarpenter.github.io/personalportfolio/"
               target="_blank"
-              rel="noopener noreferrer"
-            >
+              rel="noopener noreferrer">
               Brian DeVries
             </a>
           </p>
@@ -148,16 +119,9 @@ class App extends React.Component {
 }
 
 class Button extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
-  }
-  handleClick() {
-    this.props.onClick(this.props.value);
-  }
   render() {
     return (
-      <button id={this.props.id} onClick={this.handleClick}>
+      <button id={this.props.id} value={this.props.value} onClick={this.props.onClick}>
         {this.props.value}
       </button>
     );
